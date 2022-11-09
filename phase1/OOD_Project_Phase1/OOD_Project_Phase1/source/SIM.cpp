142b3de2e26b546464ae0f11b1d7c7a8189249a6
Mina Ashraf <mina146@aucegypt.edu>
2022-11-09 18:40:36 +0200

    better messages

--- phase1/OOD_Project_Phase1/OOD_Project_Phase1/source/SIM.cpp
+++ phase1/OOD_Project_Phase1/OOD_Project_Phase1/source/SIM.cpp
@@ -4,13 +4,13 @@
 int SIM::fetched_instructions_count = 0;
 
 SIM::SIM(int n){
-    std::cout << "Constructed SIM" << std::endl;
-    std::cout << "Number of cores: " << n << std::endl;
+    
     for (int i = 0; i < n; i++){
         cores.emplace_back(i, &data_memory);
     }
-    std::cout << "Number of cores in the SIM: " << cores.size() << std::endl;
+    std::cout << "Constructed the SIM with " << cores.size() << " cores " << std::endl;
 }
+
 void SIM::add_instructions(std::vector<Instruction*>& p_instructions, int core_id){
     cores[core_id].add_instructions(p_instructions);
 }
@@ -18,7 +18,7 @@ void SIM::add_instructions(std::vector<Instruction*>& p_instructions, int core_i
 void SIM::process(){
         // launch threads for each core
         std::vector<std::thread> threads(cores.size());
-    
+
         for(int id=0; id<cores.size(); id++){
             threads[id] = std::thread(&Core::process, &cores[id]);
         }
